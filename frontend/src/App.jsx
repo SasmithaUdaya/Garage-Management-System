6250fdf647b68881151111206d93122d9e1fc198
SasmithaUdaya <sasmithaudayasri0322@gmail.com>
2024-04-14 23:30:39 +0530

    complete create service page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import SignIn from './pages/Signin'
-import SignUp from './pages/Signup';
-import About from './pages/About';
-import Profile from './pages/Profile';
-import Header from './component/Header';
-import PrivateRoute from './component/privateRoute';
+import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import CreateListing from "./pages/CreateListing";
+import CreatePackage from "./pages/CreatePackage";
+import Header from "./component/Header";
+import PrivateRoute from "./component/privateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-service" element={<CreateListing />} />
+          <Route path="/create-package" element={<CreatePackage />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'; // Assuming you're using React Router for navigation
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className="flex justify-center mt-8"> {/* Adjust mt-8 as needed for spacing */}
+        <div>
+          <Link to="/create-service">
+            <button className="bg-slate-700 text-white rounded-lg p-3  hover:opacity-95 disabled:opacity-80 mr-4">
+              Create Service
+            </button>
+          </Link>
+          <Link to="/create-package">
+            <button className="bg-slate-700 text-white rounded-lg p-3  hover:opacity-95 disabled:opacity-80">
+              Create Package
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
